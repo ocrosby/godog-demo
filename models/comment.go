4efb93b852303bc8a0dce4843244a4670bf001c7
Omar Crosby <omar.crosby@weather.com>
2024-12-04 14:05:29 -0500

    chore: add additional models

---
+++ models/comment.go
@@ -0,0 +1,9 @@
+package models
+
+type Comment struct {
+	PostID int    `json:"postId"`
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Body   string `json:"body"`
+}
---
+++ models/photo.go
@@ -0,0 +1,10 @@
+package models
+
+struct Photo {
+	AlbumID      int    `json:"albumId"`
+	ID           int    `json:"id"`
+	Title        string `json:"title"`
+	URL          string `json:"url"`
+	ThumbnailURL string `json:"thumbnailUrl"`
+}
+
